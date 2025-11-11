bc779e451c2f4ac8cfe32e99c260d1f941b520de
Aga Kadela <agn.woz@gmail.com>
2025-11-10 16:44:33 -0800

    Add NavBar, ResumeCard, and ScoreCircle components; implement home route with resume display; remove unused welcome assets

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,44 @@
-import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import type { Route } from './+types/home';
+import NavBar from '../components/NavBar';
+import { RESUMES } from '../../constants';
+import ResumeCard from '~/components/ResumeCard';
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: 'Resume Scout' },
+    {
+      name: 'description',
+      content:
+        'Resume Scout is a tool that helps check your resume for job applications.',
+    },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <main className='bg-[url(/images/bg-main.svg)] bg-cover '>
+      <NavBar />
+      <section className='main-section'>
+        <div className='page-heading py-16'>
+          <h1> Track Your Job Applications and Ratings</h1>
+          <h2>
+            Review and improve your resume with the help of our AI-powered tool.
+          </h2>
+        </div>
+        {RESUMES.length > 0 ? (
+          <div className='resumes-section'>
+            {[
+              ...RESUMES.map((resume: Resume) => (
+                <ResumeCard key={resume.id} {...resume} />
+              )),
+            ]}
+          </div>
+        ) : (
+          <div className='resumes-section'>
+            <p>No resumes found</p>
+          </div>
+        )}
+      </section>
+    </main>
+  );
 }
